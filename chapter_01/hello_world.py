71cb5d2ba0db06b2f489c549ac4f831e3705381f
MoyoMoz <111473836+MoyoMoz@users.noreply.github.com>
2023-11-18 22:53:58 -0800

    Update hello_world.py
    added and answered try it yourself section

--- chapter_01/hello_world.py
+++ chapter_01/hello_world.py
@@ -1,1 +1,17 @@
 print("Hello Python world!")
+
+# python.org: Explore the Python home page (http://python.org/) to find topics that interest you.
+# As you become familiar with Python, different parts of the site will be more useful to you.
+
+# Hello World Typos: Open the hello_world.py file you just created. Make a typo somewhere in the line and run the program again.
+# Can you make a typo that generates an error? Can you make sense of the error message?
+# Can you make a typo that doesn't generate an error? Why do you think it didn't make an error?
+
+# Infinite Skills: If you had infinite programming skills, what would you build? You're about to learn how to program. 
+# If you have an end goal in mind, you'll have an immediate use for your new skills; 
+# now is a great time to draft descriptions of what you'd like to create.
+# It's a good habit to keep an "ideas" notebook that you can refer to whenever you want to start a new project. 
+# Take a few minutes now to describe three programs you'd like to create.
+
+print("If you had infinite programming skills I would create a device that could read soil health in a very detailed way. 
+It would give advice and instructions on different ways that the soil could be made healthier for growing nutriant dense food.") 
