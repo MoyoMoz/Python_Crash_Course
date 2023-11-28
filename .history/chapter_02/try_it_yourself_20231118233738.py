1b22fb19da34cbd3dd2ffbee97220f8a8cd189e0
MoyoMoz <moyotornai@gmail.com>
2023-11-28 12:39:23 -0800

    Added try me sections and worked through chapters 1-5

---
+++ .history/chapter_02/try_it_yourself_20231118233738.py
@@ -0,0 +1,45 @@
+# 2-1. Simple Message: Store a message in a variable, and then print that message.
+
+# message = "I'm starting the try it yourself sections all over again and that kind of sucks but now there's structure to it"
+# print(message)
+
+# 2-2. Simple Messages: Store a message in a variable, and print that message.
+# Then change the value of your variable to a new message, and print the new message.
+
+# message = "Maybe doing it this way will not only help me but someone else too. That's my hope"
+# print(message)
+
+
+# 2-3. Personal Message: Store a person's name in a variable, and print a message to that person.
+# Your message should be simple, such as, "Hello Eric, would you like to learn some Python today?"
+
+# name = "Fabio"
+# message = "You remind me of someone I used to know"
+# print(f"{name} {message}")
+
+
+# 2-4. Name Cases: Store a person's name in a variable, and then print that person's name in lowercase, uppercase, and titlecase.
+
+# name = "Fabio"
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+
+# 2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+# Your output should look something like the following, including the quotation marks:
+
+# "Albert Einstein once said, 'A person who never made a mistake never tried anything new.'"
+
+# print("Moyo once said 'I'm sleepy and I want to go to bed but I'm here instead'")
+
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time store the famous person's name in a variable called famous_person.
+# Then compose your message and store it in a new variable called message. Print your message.
+
+# famous_person = "Moyo"
+# message = "You remind me of someone I used to know"
+# print(f"{famous_person} {message}")
+
+# 2-7. Stripping Names: Store a person's name, and include some whitespace characters at the beginning and end of the name.
+# Make sure you use each character combination, "\t" and "\n", at least once.
+# Print the name once, so the whitespace around the name is displayed.
+# Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().
---
+++ .history/chapter_05/try_it_yourself_20231119222005.py
@@ -0,0 +1,217 @@
+# 5-1. Conditional Tests: Write a series of conditional tests.
+# Print a statement describing each test and your prediction for the results of each test.
+# Your code should look something like this:
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# Create at least 10 tests. Have at least 5 tests evaluate to True and another
+# 5 tests evaluate to False.
+
+# city = "Seattle"
+# print("Is city == 'Seattle' is True")
+# print(city == 'Seattle')
+
+# print("\nIs city == 'Denver' is False")
+# print(city == 'Denver')
+
+# Seattle = "PNW"
+# print("\nSeattle == 'PNW' is True")
+# print(Seattle == "PNW")
+
+# Seattle = "PNW"
+# print("\nSeattle == 'South' is Frue")
+# print(Seattle == "South")
+
+# Moyo = "sleepy"
+# print("\nMoyo == sleepy is very True")
+# print(Moyo == 'sleepy')
+
+# Moyo = "sleepy"
+# print("\nMoyo == Fresh is very False")
+# print(Moyo == 'Fresh')
+
+
+# 5-2. More Conditional Tests: You don’t have to limit the number of tests you
+# create to 10. If you want to try more comparisons, write more tests and add
+# them to conditional_tests.py. Have at least one True and one False result for each of the following:
+# Tests for equality and inequality with strings
+
+# Koyo = "Kyle + Moyo"
+# print(Koyo == "Kyle + yoyo")
+# print(Koyo != "Kyle + yoyo")
+# print(Koyo == "Kyle + Moyo")
+
+
+# Tests using the lower() function
+
+# Koyo = "Kyle + Moyo"
+# print(Koyo == "kyle + moyo")
+# print(Koyo.lower() == "kyle + moyo")
+
+# Numerical tests involving equality and inequality, greater than and less than,
+# greater than or equal to, and less than or equal to. Tests using the and keyword and the or keyword
+
+# grandpa_kyle = 34
+# print(grandpa_kyle != 23)
+# print(grandpa_kyle >= 30)
+# print(grandpa_kyle < 65)
+# print(grandpa_kyle == 33)
+# print(grandpa_kyle == 34)
+
+
+# Test whether an item is in a list
+# Test whether an item is not in a list
+
+# items = ['this', 'that', 'other', 'mother', 'father', 'baby']
+# print('this' in items)
+# print('cat' in items)
+
+
+# 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a
+# variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+# Write an if statement to test whether the alien's color is green. If it is, print
+# a message that the player just earned 5 points.
+
+# alien_color = "green"
+
+# if alien_color == 'red' or alien_color == 'yellow':
+#     print("You got 3 points")
+# elif alien_color == 'green':
+#     print("you just got the max 5 point")
+
+# Write one version of this program that passes the if test and another that fails.
+# (The version that fails will have no output.)
+
+
+# 5-4. Alien Colors #2:
+# Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+# If the alien’s color is green, print a statement that the player just earned
+# 5 points for shooting the alien.
+# If the alien’s color isn’t green, print a statement that the player just earned
+# 10 points.
+# Write one version of this program that runs the if block and another that
+# runs the else block.
+
+alien_color = "green"
+
+# if alien_color == 'red' or alien_color == 'yellow':
+#     points = 10
+# elif alien_color == 'green':
+#     points = 5
+# print(f"you just got {points} points")
+
+# alien_color = "red"
+
+# if alien_color == 'red' or alien_color == 'yellow':
+#     points = 10
+# elif alien_color == 'green':
+#     points = 5
+# print(f"you just got {points} points")
+
+# 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+# If the alien is green, print a message that the player earned 5 points.
+# If the alien is yellow, print a message that the player earned 10 points.
+# If the alien is red, print a message that the player earned 15 points.
+# Write three versions of this program, making sure each message is printed
+# for the appropriate color alien.
+
+# alien_color = "green"
+
+# if alien_color == 'green':
+#     points = 5
+# elif alien_color == 'yellow':
+#     points = 10
+# elif alien_color == 'red':
+#     points = 15
+# print(f"you just got {points} points")
+
+# alien_color = "red"
+
+# if alien_color == 'green':
+#     points = 5
+# elif alien_color == 'yellow':
+#     points = 10
+# elif alien_color == 'red':
+#     points = 15
+# print(f"you just got {points} points")
+
+# alien_color = "yellow"
+
+# if alien_color == 'green':
+#     points = 5
+# elif alien_color == 'yellow':
+#     points = 10
+# elif alien_color == 'red':
+#     points = 15
+# print(f"you just got {points} points")
+
+# 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s
+# stage of life. Set a value for the variable age, and then:
+# If the person is less than 2 years old, print a message that the person is a baby.
+# If the person is at least 2 years old but less than 4, print a message that
+# the person is a toddler.
+# If the person is at least 4 years old but less than 13, print a message that
+# the person is a kid.
+# If the person is at least 13 years old but less than 20, print a message that
+# the person is a teenager.
+# If the person is at least 20 years old but less than 65, print a message that
+# the person is an adult.
+# If the person is age 65 or older, print a message that the person is an elder.
+
+age = 37
+
+if age < 2:
+    stage = "baby"
+elif age > 4 <= 2:
+    stage = "toddler"
+elif age >= 4 < 13:
+    stage = "kid"
+elif age >= 13 < 20:
+    stage = "teenager"
+elif age >= 20 < 65:
+    stage = "adult"
+else:
+    stage = "elder"
+
+print(f"Moyo you are a {stage}")
+
+
+# 5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series
+# of independent if statements that check for certain fruits in your list.
+# Make a list of your three favorite fruits and call it favorite_fruits.
+# Write five if statements. Each should check whether a certain kind of fruit
+# is in your list. If the fruit is in your list, the if block should print a statement,
+# such as You really like bananas!
+
+# 5-8. Hello Admin: Make a list of five or more usernames, including the name 'admin'.
+# Imagine you are writing code that will print a greeting to each user after they log in
+# to a website. Loop through the list, and print a greeting to each user:
+# If the username is 'admin', print a special greeting, such as Hello admin,
+# would you like to see a status report?
+# Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+# 5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+# If the list is empty, print the message We need to find some users!
+# Remove all of the usernames from your list, and make sure the correct message is printed.
+
+# 5-10. Checking Usernames: Do the following to create a program that simulates
+# how websites ensure that everyone has a unique username.
+# Make a list of five or more usernames called current_users.
+# Make another list of five usernames called new_users. Make sure one or two of the
+# new usernames are also in the current_users list.
+# Loop through the new_users list to see if each new username has already been used.
+# If it has, print a message that the person will need to enter a new username.
+# If a username has not been used, print a message saying that the username is available.
+# Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN'
+# should not be accepted.
+
+# 5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such as
+# 1st or 2nd. Most ordinal numbers end in th, except 1,
